53b7df48212ac0ba8188735e4fd9464292367c51
jackyyuanone <jacky_yuan_one@163.com>
2016-09-11 10:06:23 +0800

    continue the function

--- learning4.py
+++ learning4.py
@@ -16,7 +16,7 @@ print(f == f2);
 print(f() == f2());
 
 
-
+#装饰器
 def log(func):
     def wrapper(*args, **kw):
         print('call %s():' % func.__name__);
@@ -26,3 +26,17 @@ def log(func):
 @log
 def now():
 	print('2016-09-10')
+
+#偏函数
+
+#默认转二进制方法
+def int2(x, base = 2):
+    return int(x, base);
+
+#functools.partial就是帮助我们创建一个偏函数的，
+#不需要我们自己定义int2()，
+#可以直接使用下面的代码创建一个新的函数int2。
+import functools;
+int2 = functools.partial(int, base = 2);
+print(int2('1000'));
+print(int2('1000', base = 10));
