1fefe8c7a0ecd5a91a7a62085cd4209a9447b9ac
JackyYuanOne <jacky_yuan_one@163.com>
2016-09-21 23:08:46 +0800

    go on learning the try catch

--- learning14.py
+++ learning14.py
@@ -46,3 +46,13 @@ def main():
 
 main();
 print('END');
+
+# 抛出错误
+class FooError(ValueError):
+	pass;
+
+def foo(s):
+	n = int(s);
+	if n== 0:
+		raise FooError("invalid value : %s" % s);
+	return 10 / n;
